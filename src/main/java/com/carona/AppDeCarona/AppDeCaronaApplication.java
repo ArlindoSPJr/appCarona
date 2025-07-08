f1169dcae9bb36debea10cd97ab243226b0e3a96
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2025-07-08 09:12:12 -0300

    Inicializando projeto

---
+++ src/main/java/com/carona/AppDeCarona/AppDeCaronaApplication.java
@@ -0,0 +1,13 @@
+package com.carona.AppDeCarona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDeCaronaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDeCaronaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carona/AppDeCarona/AppDeCaronaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carona.AppDeCarona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDeCaronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
